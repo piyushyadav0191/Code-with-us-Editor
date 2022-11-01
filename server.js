cafe8e16d3dde8894aca387e3dc21eb805f9b823
Piyush Yadav <piyushyadav0191@gmail.com>
2022-11-01 15:47:18 +0530

    Slider styles changed

--- server.js
+++ server.js
@@ -63,5 +63,5 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
